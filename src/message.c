194514ed4d3589350d2014a96622c4f6557b1799
David Goulet <dgoulet@ev0ke.net>
2015-08-18 09:32:19 +0200

    Fix: set to NULL the sendsmp pointer when handling SMP
    Also, for extra precaution, set the message pointer in
    otrl_proto_create_data() to NULL at the beginning.
    
    Fixes #72
    
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/message.c
+++ src/message.c
@@ -1502,7 +1502,7 @@ int otrl_message_receiving(OtrlUserState us, const OtrlMessageAppOps *ops,
 			    unsigned char* nextmsg;
 			    int nextmsglen;
 			    OtrlTLV *sendtlv;
-			    char *sendsmp;
+			    char *sendsmp = NULL;
 			    otrl_sm_step3(context->smstate, tlv->data,
 				    tlv->len, &nextmsg, &nextmsglen);
 
